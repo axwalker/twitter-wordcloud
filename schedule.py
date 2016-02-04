a2f22332bbde61157137a404c3dc0dd9e5dc4bac
Andrew Walker <awalkerdev@gmail.com>
2016-02-04 22:26:32 +0000

    Test heroku with short intervals

--- schedule.py
+++ schedule.py
@@ -6,7 +6,7 @@ import tweet
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=22)
+@sched.scheduled_job('interval', minutes=3)
 def scheduled_job():
     tweet.post_word_clouds()
 
