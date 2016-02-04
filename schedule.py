0dc7c52fd438bf0ae0974ddcb96fc77250d33fdc
Andrew Walker <awalkerdev@gmail.com>
2016-02-04 22:40:40 +0000

    Revert heroku schedule to daily at 11pm GMT

--- schedule.py
+++ schedule.py
@@ -6,7 +6,7 @@ import tweet
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=23)
 def scheduled_job():
     tweet.post_word_clouds()
 
